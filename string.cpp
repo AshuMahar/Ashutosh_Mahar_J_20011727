c2d24481b71e3d3de0b6229eb4146d5f785b2a41
AshuMahar <91270377+AshuMahar@users.noreply.github.com>
2021-09-25 12:30:58 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,26 @@
+//ASHUTOSH MAHAR 20011727 SEC:J
+#include<iostream>
+using namespace std;
+//void String(string s1, string s2);
+int main()
+   {
+   	string s1 ;
+   	string s2 ;
+   	cout<<"Enter line 1"<<endl;
+   	cin>>s1; 
+   	cout<<"Enter line 2"<<endl;
+   	cin>>s2; 
+   	
+   	s1.append(s2);
+  
+  	  cout << s1 ;
+   }
+ /*  void String(string s1, string s2)
+   {
+   	
+    s1.append(s2);
+  
+    cout << s1 ;
+    
+   }*/
+   	
