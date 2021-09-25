d1a826acc18f547d2760c95740dddc50bed4181b
AshuMahar <91270377+AshuMahar@users.noreply.github.com>
2021-09-25 13:51:06 +0530

    Update rotation.cpp

--- rotation.cpp
+++ rotation.cpp
@@ -8,7 +8,7 @@ int main ()
  	int n ;
  	cout<<"Enter size of array"<<endl;
  	cin>>n;
- 	int arr[n];
+ 	int arr[n+1];
  	cout<<"Enter the elements of the array "
  	for(int i = 0;i<n;i++)
  	{
@@ -16,3 +16,12 @@ int main ()
  	}
  	
  }
+void right(arr[n+1],n)
+{
+ cout<<"Enter how many spaces you wana shift right"<<end;
+ cin>>s;
+ for(int i =0;i<s;i++)
+ {
+   ar[n+1]=
+ }
+}
