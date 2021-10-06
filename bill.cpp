b85bd50d698ad4b93c5e89714427523d63eb998c
AshuMahar <91270377+AshuMahar@users.noreply.github.com>
2021-10-06 10:47:53 +0530

    Add files via upload

---
+++ bill.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float units;
+	int p;
+	cout<<"Enter no of people"<<endl;
+	cin>>p;
+	char name[p];
+
+	for(int i =0;i<p;i++)
+	{
+		cout<<"Enter user name and no of units consumed"<<endl;
+		cin>>name[i];		
+		cin>>units;
+                                                                    		
+	}
+	
+}
